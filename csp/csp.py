bb776eb6e937f1f760668c61eb6f19d3870aa2a8
Jan Mayer <jan-mayer@outlook.com>
2019-10-06 21:23:00 +0200

    fixed csp and added an example

--- csp/csp.py
+++ csp/csp.py
@@ -1,4 +1,4 @@
-from copy import deepcopy
+from copy import deepcopy, copy
 from numpy import all, any
 
 class Variable:
@@ -16,14 +16,17 @@ class Constraint:
         self.predicate = predicate
         self.bound_variables = bound_variables
 
-    def satisfied(self, variables):
-        return any([v.value is None for v in self.bound_variables]) or self.predicate(variables)
+    def satisfied(self):
+        return any([v.value is None for v in self.bound_variables]) or self.predicate(self.bound_variables)
 
 class Problem:
 
     def __init__(self, variables, constraints):
         self.variables = variables
         self.constraints = constraints
+
+    def get_variables_values(self):
+        return list(map(lambda x: x.value, self.variables))
 
     def unassigned_variables(self):
         # TODO: heuristic
@@ -48,10 +51,55 @@ class Solver:
         unassigned_variables = list(problem.unassigned_variables())
         if len(unassigned_variables) == 0:
             self.solutions.append(deepcopy(problem.variables))
-        for var in problem.unassigned_variables():
+        else:
+            var = unassigned_variables[0]
             for val in var.domain:
                 var.value = val
-                if all([c.satisfied(problem.variables) for c in problem.constraints]):
+                if all([c.satisfied() for c in problem.constraints]):
                     # TODO: forward checking
                     self._solve()
             var.value = None
+
+    def get_solution_values(self):
+        return list(map(lambda y: list(map(lambda x: x.value, y)), self.solutions))
+
+def N_queen_problem_example():
+        N = 8
+        # find a way to place 8 queens on a chessboard
+        def same_diagonal(i, j, v1, v2):
+            if j < i:
+                i, j == j, i
+            if v1 == v2 + (j - i) or v1 == v2 - (j - i):
+                pass
+            return v1 == v2 + (j - i) or v1 == v2 - (j - i)
+
+        def get_diagonal_call(i, j):
+            return lambda vars: not same_diagonal(i, j, vars[0].value, vars[1].value)
+
+        def visualise_queens(queens):
+            for q in queens:
+                print(q * 'X' + 'Q' + (N - q - 1) * 'X')
+            print()
+
+        variables = [Variable(list(range(N))) for _ in range(N)]
+        constraints = []
+        for i, v1 in enumerate(variables):
+            for j, v2 in enumerate(variables):
+                if i == j: continue
+                # same row
+                constraints.append(Constraint(lambda vars: vars[0].value != vars[1].value, [v1, v2]))
+                # diagonal
+                constraints.append(Constraint(get_diagonal_call(i, j), [v1, v2]))
+
+        s = Solver(Problem(variables, constraints))
+        s.solve()
+        print(f'found {len(s.solutions)} solutions:')
+        for v in s.get_solution_values():
+            visualise_queens(v)
+
+def main():
+    N_queen_problem_example()
+
+
+if __name__ == '__main__':
+    main()
